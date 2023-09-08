9e0d77ed6ab55904ffb228204de7cf637c47374a
irfanhilmi000 <49852728+irfanhilmi000@users.noreply.github.com>
2023-09-08 16:32:03 +0700

    ai-channel.py

---
+++ ai-channel.py
@@ -0,0 +1,108 @@
+AUTOMATIC1111
+stable-diffusion-webui
+sd-webui
+sd-webui-controlnet
+stable-diffusion-webui-images-browser
+sd-webui
+sd-webui-cutoff
+sd-webui-regional-prompter
+sd-webui-tunnels
+sd-webui-roop
+pycloudflared
+insightface==0.7.3
+onnx==1.14.0
+onnxruntime==1.15.0
+opencv-python==4.7.0.72
+tqdm
+https://github.com/AUTOMATIC1111/stable-diffusion-webui
+https://github.com/Iyashinouta/sd-model-downloader
+https://github.com/Coyote-A/ultimate-upscale-for-automatic1111
+https://github.com/Mikubill/sd-webui-controlnet
+https://github.com/A-I-Channel/public-link /content/stable-diffusion-webui/extensions/tunnels
+https://github.com/A-I-Channel/explorer-image
+/content/stable-diffusion-webui/config.json
+/content/stable-diffusion-webui/models/VAE/kl-f8-anime2.ckpt https://huggingface.co/hakurei/waifu-diffusion-v1-4/resolve/main/vae/kl-f8-anime2.ckpt
+/content/stable-diffusion-webui/models/Stable-diffusion/amanyloraCheckpoint_bakedvaeBlessedFp16.safetensors https://huggingface.co/datasets/Kizi-Art/models/resolve/main/aamAnyloraAnimeMixAnime_v1.safetensors
+/content/stable-diffusion-webui/models/Stable-diffusion/CounterfeitV30.safetensors https://civitai.com/api/download/models/57618
+/content/stable-diffusion-webui/models/VAE/vae-ft-mse-840000-ema-pruned.ckpt https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.ckpt
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_canny.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_canny_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_openpose.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_openpose_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11f1e_sd15_tile.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11f1e_sd15_tile_fp16.safetensors
+/content/stable-diffusion-webui/models/ESRGAN
+/content/stable-diffusion-webui/embeddings/embeddings.zip https://github.com/A-I-Channel/Data-file/raw/main/embeddings.zip
+/content/stable-diffusion-webui/models/ESRGAN/upscalers.zip https://huggingface.co/datasets/AI-C/Data-File/resolve/main/upscalers.zip
+/content/stable-diffusion-webui/embeddings
+/content/stable-diffusion-webui/embeddings/embeddings.zip
+embeddings.zip
+/content/stable-diffusion-webui/models/ESRGAN
+/content/stable-diffusion-webui/models/ESRGAN/upscalers.zip
+upscalers.zip
+/content
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/sonGokuDragonBallZSuper_v10.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/sonGokuDragonBallZSuper_v10.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/sonGokuDragonBallZSuper_v10.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/dbsuper_style_offset.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/dbsuper_style_offset.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/dbsuper_style_offset.jpg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/makotoShinkaiSubstyles_offset.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/makotoShinkaiSubstyles_offset.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/makotoShinkaiSubstyles_offset.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/studioGhibliStyle_offset.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/studioGhibliStyle_offset.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/studioGhibliStyle_offset.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/shingekiNoKyojinAttackOn_v1.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/shingekiNoKyojinAttackOn_v1.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/shingekiNoKyojinAttackOn_v1.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/The_Garden_of_Words.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/The_Garden_of_Words.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/The_Garden_of_Words.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/animeoutlineV4.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/animeoutlineV4.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/animeoutlineV4.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/cutelineart.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/cutelineart.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/cutelineart.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/linebaby.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/linebaby.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/linebaby.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/onePieceWanoSagaStyle_v2Offset.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/onePieceWanoSagaStyle_v2Offset.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/onePieceWanoSagaStyle_v2Offset.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.jpeg
+https://github.com/IDEA-Research/DWPose
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_canny.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_canny_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_inpaint.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_inpaint_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_openpose.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_openpose_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15s2_lineart_anime.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15s2_lineart_anime_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11f1e_sd15_tile.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11f1e_sd15_tile_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11f1p_sd15_depth.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_depth_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_seg.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_seg_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_mediapipe_face_sd15_v2.safetensors https://huggingface.co/lokCX/control_mediapipe_face/resolve/main/control_mediapipe_face_sd15_v2.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11e_sd15_ip2p.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11e_sd15_ip2p_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11e_sd15_shuffle.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11e_sd15_shuffle_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_lineart.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_lineart_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_mlsd.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_mlsd_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_normalbae.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_normalbae_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_scribble.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_scribble_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_softedge.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_softedge_fp16.safetensors
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_canny_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_canny_sd14v1.pth
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_color_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_color_sd14v1.pth
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_depth_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_depth_sd14v1.pth
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_keypose_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_keypose_sd14v1.pth
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_openpose_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_openpose_sd14v1.pth
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_seg_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_seg_sd14v1.pth
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_sketch_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_sketch_sd14v1.pth
+/content/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_style_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_style_sd14v1.pth
+https://github.com/A-I-Channel/sdw-wd14-tagger
+https://github.com/fkunn1326/openpose-editor
+https://github.com/A-I-Channel/sd-openpose-editor-clone
+-b v2.5 https://github.com/camenduru/ui /content/stable-diffusion-webui
+https://github.com/A-I-Channel/ui-clone /content/stable-diffusion-webui
+https://github.com/A-I-Channel/AI_Channel_Theme
+https://github.com/deforum-art/sd-webui-deforum
+https://github.com/Bing-su/adetailer
+https://github.com/hnmr293/sd-webui-cutoff
+https://github.com/Gourieff/sd-webui-reactor
+https://huggingface.co/datasets/AI-C/Data-File/resolve/main/inswapper_128.onnx
